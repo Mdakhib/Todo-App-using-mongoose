e21980f4293e168264c6b211f93c7b272d67242f
mdakhib <mohamedakhib@gmail.com>
2020-09-21 16:47:49 +0530

    connecting with mongoDB is Done

--- app.js
+++ app.js
@@ -1,20 +1,60 @@
 const express=require("express");
 const bodyParser=require("body-parser");
-const date=require(__dirname+"/date.js");
+const mongoose=require("mongoose")
 const app=express()
 
 
-const items=["Khana","Peena","Sona"];
-const workItems=[]
+// const items=["Khana","Peena","Sona"];
+// const workItems=[]
+
+
 
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
+
+
+
+mongoose.connect("mongodb://localhost:27017/todolistDB",{ useNewUrlParser: true, useUnifiedTopology: true } )
+
+const itemsSchema={
+    name:String
+}
+
+const Item=mongoose.model("Item",itemsSchema)
+
+const item1=new Item({
+    name:"Welcome to your todoList"
+});
+
+const item2=new Item({
+    name:"Hit the + button to add a new Item"
+});
+
+const item3=new Item({
+    name:"<-- Hit this to delete an item"
+});
+
+
+const defaultItems=[item1,item2,item3];
+
+
+// Item.insertMany(defaultItems,function(err){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("successfull");
+//     }
+// })
+
+
+
 app.get("/",(req,res)=>{
-    const day=date.getDate();
+
+    
     res.render("list",{
-        listTitle:day,
+        listTitle:"Today",
         newListItems:items
     });
 });
