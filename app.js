bb3cfdfc7d5f8702cc5314eefc833368837387f3
mdakhib <mohamedakhib@gmail.com>
2020-09-22 16:59:22 +0530

    added version of NODE and added gitignore,Procfile and ports

--- app.js
+++ app.js
@@ -149,7 +149,10 @@ app.post("/delete",function(req,res){
 
 
 
-
-app.listen(process.env.PORT || 3000,()=>{
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+app.listen(port, () => {
     console.log("server is running at 3000");
 }) 
